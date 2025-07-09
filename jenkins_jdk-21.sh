019a5c673e3d8922fff9e703f786dd986cd7eb36
Rahul <50007391+rahulsaikasarla@users.noreply.github.com>
2025-07-09 23:34:45 +0530

    Update jenkins_jdk-21.sh

--- jenkins_jdk-21.sh
+++ jenkins_jdk-21.sh
@@ -9,7 +9,10 @@ sudo apt install fontconfig openjdk-21-jre -y
 java --version
 sleep 10
 
+
 #jenkins installation
+echo "jenkins installation"
+
 sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian/jenkins.io-2023.key
 echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
