5371b1c6ea0c8853c28bd34e4e101e37de1163be
Rahul <50007391+rahulsaikasarla@users.noreply.github.com>
2025-07-10 12:20:55 +0530

    jenkins_with_jdk-17.sh
    This file as jenkins with jdk-17

---
+++ jenkins_with_jdk-17.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo apt update
+echo "Installing Jdk-17"
+sleep 10
+sudo apt install fontconfig openjdk-17-jre -y
+java --version
+sleep 10
+echo "adding java binary"
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sleep 10
+echo "installing jenkins"
+sudo apt-get install jenkins -y
